6da73e7bf6156f255be9fa97eb67ed40eba262a6
diegxperez <diegouceda9@gmail.com>
2024-02-12 22:57:57 -0500

    Day 2

--- hello_world_app/lib/main.dart
+++ hello_world_app/lib/main.dart
@@ -1,16 +1,19 @@
 import 'package:flutter/material.dart';
 
-void main(){
+void main() {
   runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
+  const MyApp({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      home: Text("Hola Mundo"),
+    return const MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        body: Center(child: Text("Hola Mundo")),
+      )
     );
   }
-
 }
