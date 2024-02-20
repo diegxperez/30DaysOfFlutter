467a86ab6c250ef337247d647c2feee7b49b4e40
diegxperez <diegouceda9@gmail.com>
2024-02-19 23:03:57 -0500

    Day 6

--- DevCamp2024/Session_01/plant_app/lib/main.dart
+++ DevCamp2024/Session_01/plant_app/lib/main.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 
 
 void main() {
   runApp(const MyApp());
@@ -11,20 +10,57 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-        debugShowCheckedModeBanner: false,
-        home: Scaffold(
-          appBar: AppBar(title: const Text('App Plant')),
-          body: const Center(
-            child: Text('Hi'),
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+          body: Column(children: [
+        Container(
+          padding: EdgeInsets.only(top: 20),
+          color: Colors.green[400],
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              Container(
+                margin: const EdgeInsets.only(left: 10),
+                child: const Icon(Icons.arrow_back_ios_new),
+              ),
+              Container(
+                margin: const EdgeInsets.only(right: 10),
+                child: const Icon(Icons.more_horiz_outlined),
+              )
+            ],
           ),
-        ));
+        ),
+        ClipPath(
+          clipper: MyCustomClipper(),
+          child: Container(
+            color: Colors.green[400],
+            height: 250,
+          ),
+        ),
+      ])),
+    );
   }
 }
 
 class MyCustomClipper extends CustomClipper<Path> {
   @override
   Path getClip(Size size) {
-    Path path = _getStarPath();
+    double w = size.width;
+    double h = size.height;
+
+    final path = Path();
+
+    // (0,0) // 1.Point
+    path.lineTo(0, h - 100); // 2.Point
+    path.quadraticBezierTo(
+      w * 0.5, // 3. Point
+      h + 100, // 3. Point
+      w, // 4. Point
+      h - 100, // 4. Point
+    );
+    path.lineTo(w, 0); // 5.Point
+    path.close();
+
     return path;
   }
 
